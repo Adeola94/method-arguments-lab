412353707f08372091676aaddc4dad25aef920e4
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-11 22:14:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,11 @@
-def introduction = ("Adeola")
-  phrase = introduction 
+def introduction = (name)
   puts "Hi, my name is #{name}.\n"
 end 
+
+introduction ("Adeola")
 
 def introduction = ( name, language )
   puts "Hi, my name is #{name}, and i am learning #{language}."
 end 
+
+introduction ("Adeola", "Ruby")
