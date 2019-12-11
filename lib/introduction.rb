f6f5af9350d204198c5520f4d160202ce515c2c5
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-11 22:11:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,6 @@ def introduction = ("Adeola")
   puts "Hi, my name is #{name}.\n"
 end 
 
-def introduction = ( name = "Adeola", program )
-  puts "Hi, my name is #{name}, and i am learning #{program}."
+def introduction = ( name = "Adeola", language )
+  puts "Hi, my name is #{name}, and i am learning #{language}."
 end 
