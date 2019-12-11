df7ed126f8e0ef0b9f87897616d9ddbc91b8956d
Adeola Odeyemi <dbrizomomi@icloud.com>
2019-12-11 22:15:11 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+def introduction = (name)
+  puts "Hi, my name is #{name}.\n"
+end 
+
+introduction ("Adeola")
+
+def introduction = ( name, language )
+  puts "Hi, my name is #{name}, and i am learning #{language}."
+end 
+
+introduction ("Adeola", "Ruby")
